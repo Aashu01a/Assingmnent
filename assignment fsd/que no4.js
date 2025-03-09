433f0d0bff7e57767daea9daf05e9e239ac066cb
Aashu01a <120313158+Aashu01a@users.noreply.github.com>
2025-03-09 14:07:56 +0530

    Add files via upload

---
+++ assignment fsd/que no4.js
@@ -0,0 +1,24 @@
+if(true) 
+{
+    let x = 10;
+    const y = 20;
+    console.log(x, y); // output will be 10 20
+}
+
+const name = "Jean";
+const age = 21;
+console.log(`Hello, my name is ${name} and my age is ${age}`); 
+
+if (true) {
+    var a = 10;
+    var b = 20;
+    console.log(a, b); 
+}
+
+console.log(a, b); 
+
+//var a = 5;
+//var a = 10;  (allowed)
+
+//let b = 5;
+//let b = 10;   not allowed 'b' has already been declared
