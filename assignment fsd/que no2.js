40caa639bb879e518c4f56e563744ed5e057e283
Aashu01a <120313158+Aashu01a@users.noreply.github.com>
2025-03-09 14:09:33 +0530

    Delete assignment fsd directory

--- assignment fsd/que no2.js
+++
@@ -1,6 +0,0 @@
-function repeatString(str, times = 10) {
-    return str.repeat(times);
-}
-
-console.log(repeatString("Hello", 5));   
-console.log(repeatString("Hi"));         
--- assignment fsd/que no3.js
+++
@@ -1,5 +0,0 @@
-const sumArray = (arr) => arr.reduce((sum, num) => sum + num, 0);
-
-console.log(sumArray([1, 2, 3, 4, 5])); 
-console.log(sumArray([10, 20, 30]));    
-console.log(sumArray([]));              
